9b99cf1c991b4abdeee407f8490e749ba88343f7
prashant9315 <137028444+prashant9315@users.noreply.github.com>
2024-05-12 21:26:45 +0530

    Delete 18 Fragments directory

--- 18 Fragments/learning-fragments/src/assets/Components/Container.jsx
+++
@@ -1,6 +0,0 @@
-import styles from "./Container.module.css";
-
-const Container = (props) => {
-  return <div className={styles.containerbox}>{props.children}</div>;
-};
-export default Container;
--- 18 Fragments/learning-fragments/src/assets/Components/ErrorMessage.jsx
+++
@@ -1,5 +0,0 @@
-const ErrorMessage = ({ llm }) => {
-  return <>{llm.length === 0 && <h1>i am hungry</h1>}</>;
-};
-
-export default ErrorMessage;
